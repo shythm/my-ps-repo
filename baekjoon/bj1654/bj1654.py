5d5de44c6d23ce9c962a18cbec244ce8a90bceaa
shythm <55665829+shythm@users.noreply.github.com>
2020-07-28 00:09:46 +0900

    solve bj1654

--- baekjoon/bj1654/bj1654.py
+++ baekjoon/bj1654/bj1654.py
@@ -6,14 +6,18 @@ for i in range(K):
     L.append(int(stdin.readline()))
 
 left = 1
-right = min(L) + 1 # +1: to catch maximum
-while (left != right):
-    mid = (left + right) // 2
-    count = sum([l // mid for l in L])
-    # print(left, mid, right, ':', count)
-    if (count >= N):
-        left = mid + 1
-    else:
-        right = mid
-    
-stdout.write(str(left - 1))
+right = max(L)
+
+if (sum([l // right for l in L]) >= N):
+    stdout.write(str(right))
+else:
+    while (left != right):
+        mid = (left + right) // 2
+        count = sum([l // mid for l in L])
+        # print(left, mid, right, ':', count)
+        if (count >= N):
+            left = mid + 1
+        else:
+            right = mid
+
+    stdout.write(str(left - 1))
